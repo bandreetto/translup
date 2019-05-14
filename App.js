702ceb5e4b12d6db6d425379401f5f932f262ae7
Bruno Andreetto <bruno.andreetto@usp.br>
2019-05-14 12:04:03 -0300

    Add first screen

--- App.js
+++ App.js
@@ -1,40 +1,77 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Image, Text } from "react-native";
+import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
-    state = {
-      cameraPermissionGranted: false,
-      type: Camera.Constants.Type.back,
-    }
+  state = {
+    cameraPermissionGranted: false
+  };
 
   async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA)
-    this.setState({ cameraPermissionGranted: status === 'granted' })
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ cameraPermissionGranted: status === "granted" });
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        {this.state.cameraPermissionGranted && 
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+      <View style={{ flex: 1 }}>
+        {this.state.cameraPermissionGranted ? (
+          <Camera style={{ flex: 1 }}>
             <View
               style={{
                 flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}/>
-          </Camera>}
+                backgroundColor: "transparent",
+                flexDirection: "column"
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  marginTop: 30,
+                  marginLeft: 5
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                  });
+                }}
+              >
+                <Image source={require("./assets/gear.png")} />
+              </TouchableOpacity>
+              <View
+                style={{
+                  flex: 1,
+                  marginTop: 120
+                }}
+              >
+                <Text
+                  style={{
+                    marginLeft: 75,
+                    backgroundColor: "white",
+                    width: 50
+                  }}
+                >
+                  Cadeira
+                </Text>
+                <View
+                  style={{
+                    borderWidth: 5,
+                    borderColor: "white",
+                    marginLeft: 75,
+                    marginRight: 75,
+                    height: 300
+                  }}
+                />
+              </View>
+            </View>
+          </Camera>
+        ) : (
+          <View />
+        )}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
