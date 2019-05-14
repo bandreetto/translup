4aa32ef1f75d525b336d80852a5119a1b4de9d95
Bruno Andreetto <bruno.andreetto@usp.br>
2019-05-14 13:03:44 -0300

    Add settings content

--- Settings.js
+++ Settings.js
@@ -1,8 +1,66 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, Picker } from "react-native";
 
 export class Settings extends React.Component {
+  static navigationOptions = {
+    title: "Settings"
+  };
   render() {
-    return <Text>Settings</Text>;
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDitection: "column",
+          alignItems: "center",
+          justifyContent: "space-evenly"
+        }}
+      >
+        <View>
+          <Text>I speak</Text>
+          <Picker
+            selectedValue="en"
+            style={{
+              height: 50,
+              width: 150,
+              borderWidth: 5,
+              borderColor: "black"
+            }}
+          >
+            <Picker.Item label="Portuguese" value="pt" />
+            <Picker.Item label="English" value="en" />
+          </Picker>
+        </View>
+        <View>
+          <Text>I want to learn</Text>
+          <Picker
+            selectedValue="pt"
+            style={{
+              height: 50,
+              width: 150,
+              borderWidth: 5,
+              borderColor: "black"
+            }}
+          >
+            <Picker.Item label="Portuguese" value="pt" />
+            <Picker.Item label="English" value="en" />
+          </Picker>
+        </View>
+        <View>
+          <Text>I want to learn about</Text>
+          <Picker
+            selectedValue="things"
+            style={{
+              height: 50,
+              width: 150,
+              borderWidth: 5,
+              borderColor: "black"
+            }}
+          >
+            <Picker.Item label="Things" value="things" />
+            <Picker.Item label="Animals" value="animals" />
+          </Picker>
+        </View>
+      </View>
+    );
   }
 }
